3cced7e34863f2776e9b902b270ed8bb399f6867
muhammadqosim <qosimmuhammad1961@gmail.com>
2021-04-02 17:48:26 +0800

    Memodifikasi File Input & Output

--- InputOutput.java
+++ InputOutput.java
@@ -1,12 +1,24 @@
-import java.ulti.Scanner;
+import java.util.Scanner;
 
-public class Input{
+public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  String npm;
+  String kelas;
+
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
-  System.out.println("Hai, "+nama);
+ 
+  System.out.print("Masukkan npm anda: ");
+  npm = scanner.nextLine();
 
+  System.out.print("Masukkan kelas anda: ");
+  kelas = scanner.nextLine();
+  
+  System.out.println();
+  System.out.println("Nama : "+nama);
+  System.out.println("NPM : "+npm);
+  System.out.println("Kelas : "+kelas);
  }
 }
